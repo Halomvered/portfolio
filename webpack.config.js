027a79d366e1edeb80179e8f971d06a1bf62fb04
Halom Vered <halom.vered@gmail.com>
2018-01-03 13:43:15 +0200

    Header/Side-navigation styling for pc complete.

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
                 use: ['html-loader']
             },
             {
-                test: /\.(jpg|png)$/,
+                test: /\.(jpg|png|svg)$/,
                 use: [
                     {
                         loader: 'file-loader',
