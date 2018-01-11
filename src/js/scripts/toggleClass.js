6f6212d135247a3f51ed15549631b5cf85776952
Halom Vered <halom.vered@gmail.com>
2018-01-11 17:32:16 +0200

    toggleClass script refactored to include the call to action button - which if now pressed, highlights the contact icon on the sidenav.

--- src/js/scripts/toggleClass.js
+++ src/js/scripts/toggleClass.js
@@ -3,14 +3,27 @@ export var toggleClass = function () {
     // Highlight current icon for PC sidenav.
     document.addEventListener('click', e => {
         var target = e.target;
-        // Event delegation to target only clicks on sidenav.
-        if (target.getAttribute("class") === 'btn-js__invisible') {
+        
+        // Event delegation to target only clicks on sidenav or the call to action button.
+        if (target.getAttribute("class") === 'btn-js__invisible' || 
+        target.getAttribute("class").includes('btn-higlight-js')) {
+
+            // All sidenav items are stored in a var.
+            var sideNavItems = [...document.querySelectorAll('.btn-js__invisible')];
+
             // Turn off all current highlighted items.
-            [...document.querySelectorAll('.btn-js__invisible')].forEach(element => {
-                element.previousElementSibling.classList.remove('visible_on-js');
+            sideNavItems.forEach(element => {
+                element.previousElementSibling.classList.remove('highlight-nav--js');
             });
-            // Toggle the relevant class on the SVG element of the clicked item.
-            target.previousElementSibling.classList.toggle('visible_on-js');
+
+            // If the call to action button was called, toggle the active class on the contact icon in the side nav.
+            if (target.getAttribute("class").includes('btn-higlight-js')) {
+                sideNavItems[sideNavItems.length - 1].previousElementSibling.classList.toggle('highlight-nav--js');
+            } else {
+                // Toggle the relevant class on the SVG element of the clicked item.
+                target.previousElementSibling.classList.toggle('highlight-nav--js');
+            }
+            
         }
     });
 }
