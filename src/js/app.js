28c670c6877bfaf28debd4c5499e86f5de0cb1b1
Halom Vered <halom.vered@gmail.com>
2018-01-15 18:11:46 +0200

    Front-end part of the site, minus the media queries, is fully complete.

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,23 @@
 import '../css/main.scss';
 import '../contentscript.html';
-import {randomFadingCircles} from './scripts/randomFadingCircles.js';
-import {canvasWriting} from './scripts/canvasWriting.js';
-import {clickAndDrag} from './scripts/clickAndDrag.js';
-import {toggleClass} from './scripts/toggleClass.js';
+import {randomFadingCircles} from './scripts/randomcircles.js';
 
-// SETUP FOR USING randomFadingCircles
+// SETUP FOR USING randomFadingCircles()
 // Insert the class of the canvas element you'd like to work with.
-var elementMain = '.main-content__canvas';
+var element = '.main-content__canvas';
 // // Set the maximum radius that a circle can grow to.
 // var maxRadius = 40;
 // // Set the minimum radius that a circle can shrink to.
 // var minRadius = 20;
 // // Set the range of size the of each circle is generated as.
-var radiusRange = 2;
+var radiusRange = 1.5;
 // // Set the range of size the of each circle is generated as.
-var changeRate = 0.006;
+var changeRate = 0.005;
 // Define an array of colors from which the circles are randomly generated.
-var colorsMain = [
+var colorArray = [
     "rgba(255, 255, 255, 0.055)",
 ];
 var circleNum = 80;
 
-
-// SETUP FOR USING canvasWriting
-// Insert the class of the canvas element you'd like to work with.
-var elementPopup = '.popup__canvas';
-// Define the minimum font size that will be drawn on the canvas.
-var minFontSize = 15;
-// Set the text font.
-var canvasfont = "Roboto";
-// Set the text that will be drawn on the canvas.
-var letters = "Javascript, CSS, HTML, Sass, Node, Express, MongoDB, Version Control, Webpack, Jqeury, Ajax, API, REST, Illustrator, Photoshop, Bootstrap.";
-
-// SETUP FOR USING clickAndDrad
-// Insert the speed in which you want to scroller to move.
-var scrollSpeed = 1.5;
-
-window.addEventListener('load', ev => {
-    randomFadingCircles(elementMain, radiusRange, changeRate, colorsMain, circleNum);
-    canvasWriting(elementPopup, minFontSize, canvasfont, letters);
-    clickAndDrag(scrollSpeed);
-    toggleClass();
-});
+;
+randomFadingCircles(element, radiusRange, changeRate, colorArray, circleNum);
