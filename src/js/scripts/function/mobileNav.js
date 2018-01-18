ed729ea9574490f75c5438f8765067337e32b660
Halom Vered <halom.vered@gmail.com>
2018-01-18 16:41:54 +0200

    Event delegation removed from mobileNav.js and toggleNavHighlight.js and replaced by an evenetlistener placed directly above the relevant buttons.

--- src/js/scripts/function/mobileNav.js
+++ src/js/scripts/function/mobileNav.js
@@ -1,18 +1,11 @@
 export var mobileNav = function () {
 
      // Activate/ deactivate mobile nav.
-     document.addEventListener('click', e => {
-        var target = e.target;
-            
-        // Event delegation to target only clicks on nav-icon.
-        if (target.parentElement.getAttribute("class") === 'nav-icon' ||
-            target.getAttribute("class") === 'nav-icon' ||
-            target.parentElement.getAttribute("class").includes('nav-icon') ||
-            target.getAttribute("class").includes('nav-icon')) {
-                    
+     document.querySelector('.nav-icon').addEventListener('click', e => {
+        
             // Set nav-icon and the moile nav to active.    
             document.querySelector('.nav-icon').classList.toggle('active');
             document.querySelector('.sidebar__sidenav').classList.toggle('active');
-        }
+        
     });
 }
--- src/js/scripts/function/toggleNavHighligh.js
+++ src/js/scripts/function/toggleNavHighligh.js
@@ -1,31 +1,29 @@
 export var toggleNavHighligh = function () {
 
-    // Highlight current icon for PC sidenav.
-    document.addEventListener('click', e => {
-        var target = e.target;
-        
-        // Event delegation to target only clicks on sidenav, call to action button, or window close buttons.
-        if (target.getAttribute("class").includes('navMarker--js')) {
-
-            // All sidenav icons are stored in a var.
-            var sideNavItems = [...document.querySelectorAll('.btn-js__visible')];
+    // All sidenav icons are stored in a var.
+    var sideNavItems = [...document.querySelectorAll('.btn-js__visible')];
+    var buttons = document.querySelectorAll('.navMarker--js');
+    
+    buttons.forEach( button => {
+        // Highlight current icon for PC sidenav.
+        button.addEventListener('click', e => {
 
             // Turn off all current highlighted items.
             sideNavItems.forEach(element => {
                 element.classList.remove('highlight-nav--js');
             });
-
             // If a side-nav button was pressed, highlight it.
-            if (target.getAttribute("class").includes('btn-js__invisible')) {
-                target.previousElementSibling.classList.toggle('highlight-nav--js');
+            if (button.getAttribute("class").includes('btn-js__invisible')) {
+                button.previousElementSibling.classList.toggle('highlight-nav--js');
             // If the call to action button was pressed highlight the contact icon.
-            } else if (target.getAttribute("class").includes('cta--js')) {
+            } else if (button.getAttribute("class").includes('cta--js')) {
                 sideNavItems[sideNavItems.length - 1].classList.toggle('highlight-nav--js');
             // if the close-window button was pressed, highlight the home icon.
             } else {
                 sideNavItems[0].classList.toggle('highlight-nav--js');
             }
-            
-        }
+        });
     });
+
+    
 }
